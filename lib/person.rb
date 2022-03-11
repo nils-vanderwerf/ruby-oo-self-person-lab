caf5290f73eb403dafc19e84a814a6e4dd950985
Nils van der Werf <n.vanderw.92@gmail.com>
2022-03-11 17:57:52 +1100

    Written all methods which increase or decrease hygiene and happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 # your code goes here
+require 'pry'
 class Person
   attr_accessor :bank_account, :happiness, :hygiene
   attr_reader :name
@@ -28,4 +29,44 @@ class Person
   def happy?
     @happiness > 7
   end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
 end
+
+bob = Person.new("Bob")
+felix = Person.new("Felix")
+bob.call_friend(felix)
